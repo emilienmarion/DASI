1e187d8d7a413a8440f93823a64a8e54af08ecc5
Ithanvelreq <ithanvelreq@gmail.com>
2021-05-18 20:15:32 +0200

    nouvelle action

---
+++ frontend/src/main/java/action/ObtenirConsultations.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import dao.UtilisateurDao;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import metier.modele.*;
+import metier.service.Service;
+
+/**
+ *
+ * @author ithan
+ */
+public class ObtenirConsultations extends Action{
+
+    @Override
+    public void executer(HttpServletRequest request) {
+        System.out.println("je suis dans l'action obtenirConsultations");
+        //
+        //
+        //A changer apres avoir discuter!!!!!!!!!!!!!!!
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        Service service = new Service();
+        Utilisateur u = service.chercherClient(nom, prenom);
+        //ATENTION!!!!!!!!!!!!!!
+        //
+        //
+        boolean connex = false;
+        List<Consultation> listeCons = null;
+        
+        if(u instanceof Client){
+            Client c = (Client) u;
+            listeCons = c.getConsultations();
+            connex = true;
+        }else if(u instanceof Employe){
+            Employe e = (Employe) u;
+            listeCons = e.getConsultations();
+            connex = true;
+        }     
+        
+        System.out.println(u);
+        request.setAttribute("connexion", connex);
+        request.setAttribute("utilisateur",u );
+        request.setAttribute("consultation", listeCons);
+    }
+    
+}
